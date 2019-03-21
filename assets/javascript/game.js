989f9b604dc03f88991a3ba13a802a0935d7472f
John and Kari <bryceacampbell7@gmail.com>
2019-03-20 23:55:40 -0700

    added questions, btn hover effects & more styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
 
+    //global variables - to be used anywhere...mehhh need to use less global variable for best practices
+    var correctAnswerCounter = 0;
+    var wrongAnswerCounter = 0;
+    var unansweredCounter = 0;
+    var timer = 20;
+    var intervalId;
+    var userGuess = "";
+    var running = false;
+
     // Array of objects containing questions, choices, correct answer, and image urls
     var questions = [
         {
@@ -32,16 +41,39 @@ $(document).ready(function () {
             answer: 0,
             image: "assets/images/Jim.png"
         },
+        {
+            question: "Who is Romani?",
+            choice: ["The girl from Lon Lon Ranch in Ocarina of Time", "The inn Keeper in Majora's Mask", "Creamia's younger sister", "The elder sister that lives on the ranch in Majora's Mask"],
+            answer: 2,
+            image: "assets/images/Romani.png"
+        },
+        {
+            question: "Who is the current producer of The Legend of Zelda series?",
+            choice: ["Koji Kondo", "Eiji Aonuma", "Shigeru Miyamoto", "Satoru Iwata"],
+            answer: 1,
+            image: "assets/images/the-master.png"
+        },
+        {
+            question: "In Super Smash Bros. Brawl, what is the name of Link's Final Smash?",
+            choice: ["Triforce of Courage", "Master Sword Attack", "Triforce Slash", "Hylian Blitz"],
+            answer: 2,
+            image: "assets/images/Triforce-Slash.jpg"
+        },
+        {
+            question: "The Legend of Zelda is notable for being the first video game that...?",
+            choice: ["Allowed players to save", "Sold over a million copies on the NES", "Was scored 40/40 by Famitsu Magazine", "All of the above"],
+            answer: 0,
+            image: "assets/images/Save-icon.png"
+        },
+        {
+            question: "In A Link to the Past, which of the following places will the flute NOT take Link?",
+            choice: ["Death Mountain", "Links House", "The Pyramid of Power", "The Lost Woods"],
+            answer: 3,
+            image: "assets/images/master-sword.png"
+        },
     ];
 
-    //global variables - to be used anywhere
-    var correctAnswerCounter = 0;
-    var wrongAnswerCounter = 0;
-    var unansweredCounter = 0;
-    var timer = 20;
-    var intervalId;
-    var userGuess = "";
-    var running = false;
+    //more global variables for manipulating the questions array and splice them out and into new arrays
     var questionCounter = questions.length;
     var questionSelected;
     var currentQuestion;
@@ -51,11 +83,13 @@ $(document).ready(function () {
     //hide reset button at document on ready
     $("#reset-game").hide();
     $(".container").addClass("container-clear");
+    $("#time-remaining").hide()
 
     //click start button to start game
     $("#start-game").on("click", function () {
         $("#start-game").hide();
         $(".container").removeClass("container-clear");
+        $("#time-remaining").show();
         questionsDisplay();
         runTimer();
         for (var i = 0; i < questions.length; i++) {
@@ -72,7 +106,7 @@ $(document).ready(function () {
         questionSelected = questions[currentQuestion];
 
         //display selected question
-        $("#questionblock").html("<h2>" + questionSelected.question + "</h2>");
+        $("#question-display").html("<h2>" + questionSelected.question + "</h2>");
 
         //for loop to display possible choices
         for (var i = 0; i < questionSelected.choice.length; i++) {
@@ -80,7 +114,7 @@ $(document).ready(function () {
             userChoice.addClass("answerchoice");
             userChoice.html(questionSelected.choice[i]);
             userChoice.attr("data-guessvalue", i);
-            $("#answerblock").append(userChoice);
+            $("#answer-display").append(userChoice);
         }
 
         //click function to select answer and run outc
@@ -94,14 +128,14 @@ $(document).ready(function () {
                 stop();
                 correctAnswerCounter++;
                 userGuess = "";
-                $("#answerblock").html("<p>Correct!</p>");
-                outcome();  
-            } 
+                $("#answer-display").html("<p>Correct!</p>");
+                outcome();
+            }
             else {
                 stop();
                 wrongAnswerCounter++;
                 userGuess = "";
-                $("#answerblock").html("<p>Wrong! The correct answer is: " + questionSelected.choice[questionSelected.answer] + "</p>");
+                $("#answer-display").html("<p>Wrong! The correct answer is: " + questionSelected.choice[questionSelected.answer] + "</p>");
                 outcome();
             }
         })
@@ -117,19 +151,19 @@ $(document).ready(function () {
 
     //function to display timer countdown
     function decrement() {
-        $("#timeleft").html("<h3>Time remaining: " + timer + "</h3>");
+        $("#time-remaining").html("<h3>Time remaining: " + timer + "</h3>");
         timer--;
 
-        //if timer reaches 0, stop timer
+        //if timer reaches 0, stop timer and display time is up
         if (timer === 0) {
             unansweredCounter++;
             stop();
-            $("#answerblock").html("<p>Time is up! The correct answer is: " + questionSelected.choice[questionSelected.answer] + "</p>");
+            $("#answer-display").html("<p>Time is up! The correct answer is: " + questionSelected.choice[questionSelected.answer] + "</p>");
             outcome();
         }
     }
 
-    //stop the time
+    //stop the timer
     function stop() {
         running = false;
         clearInterval(intervalId);
@@ -137,7 +171,7 @@ $(document).ready(function () {
 
     //function to display images and splice the question selected out of the array so it wont be used again
     function outcome() {
-        $("#answerblock").append("<img src=" + questionSelected.image + ">");
+        $("#answer-display").append("<img src=" + questionSelected.image + ">");
 
         //pushing question selected into a new array
         finishedQuestionsArray.push(questionSelected);
@@ -145,23 +179,23 @@ $(document).ready(function () {
         //splice then removes it out of the inital array 
         questions.splice(currentQuestion, 1);
 
-            setTimeout(function () {
-            $("#answerblock").empty();
+        setTimeout(function () {
+            $("#answer-display").empty();
             timer = 20;
-    
+
             //run the score screen if all questions answered
             if ((wrongAnswerCounter + correctAnswerCounter + unansweredCounter) === questionCounter) {
-                $("#questionblock").empty();
-                $("#questionblock").html("<h3>Game Over!  Here's your results: </h3>");
-                $("#answerblock").append("<h4> Correct: " + correctAnswerCounter + "</h4>");
-                $("#answerblock").append("<h4> Incorrect: " + wrongAnswerCounter + "</h4>");
-                $("#answerblock").append("<h4> Unanswered: " + unansweredCounter + "</h4>");
+                $("#question-display").empty();
+                $("#question-display").html("<h3>Game Over!  Here's your results: </h3>");
+                $("#answer-display").append("<h4> Correct: " + correctAnswerCounter + "</h4>");
+                $("#answer-display").append("<h4> Incorrect: " + wrongAnswerCounter + "</h4>");
+                $("#answer-display").append("<h4> Unanswered: " + unansweredCounter + "</h4>");
                 $("#reset-game").show();
                 correctAnswerCounter = 0;
                 wrongAnswerCounter = 0;
                 unansweredCounter = 0;
 
-            } 
+            }
             else {
                 runTimer();
                 questionsDisplay();
@@ -172,8 +206,8 @@ $(document).ready(function () {
     //reset game on click function to reset the game
     $("#reset-game").on("click", function () {
         $("#reset-game").hide();
-        $("#answerblock").empty();
-        $("#questionblock").empty();
+        $("#answer-display").empty();
+        $("#question-display").empty();
         for (var i = 0; i < holderArray.length; i++) {
             questions.push(holderArray[i]);
         }
